66255ccc030f570fc39885bd4a1d7f61e21cfb45
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-05-15 00:44:55 +0930

    Update Spiker.cpp
    Sry

---
+++ Spiker.cpp
@@ -0,0 +1,21 @@
+#include "Spiker.h"
+using namespace std;
+
+Spiker::Spiker(){
+}
+
+int Spiker::attemptServe(){
+  
+}
+
+int Spiker::attemptDig(){
+  
+}
+
+int Spiker::attemptSet(){
+  
+}
+
+int Spiker::attemptSpike(){
+  
+}
