534f9e66a08767bc95ff94eb49e1f44240ce14b1
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-05-15 00:42:11 +0930

    Update Digger.cpp
    im

---
+++ Digger.cpp
@@ -0,0 +1,21 @@
+#include "Digger.h"
+using namespace std;
+
+Digger::Digger(){
+}
+
+int Digger::attemptServe(){
+
+}
+
+int Digger::attemptDig(){
+  
+}
+
+int Digger::attemptSet(){
+  
+}
+
+int Digger::attemptSpike(){
+  
+}
