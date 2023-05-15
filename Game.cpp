fd2d551dea24a0d42bcd449248878d9b384ace25
Marvin <marvin.fernandez7046@gmail.com>
2023-05-15 23:50:25 +0930

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -32,11 +32,25 @@ void Game::play(){ // mic
 while(scoreTeam1 < winPoints && scoreTeam2 < winPoints) {
     
     // simulate one point
-    int servingTeamIndex = 0;   // team 1 scores first
+    int servingTeamIndex = 0;   // team 1 serves first
     bool action = true;
 
     while(action == true) {
         Team* servingTeam = team1
+        // player 6 serves first (back right)
+        // team1[6].attemptServe   
+
+
+        // dig team 2
+        // if setter in position 4,5,6 (generate a number between 4,5,6 excluding setter position)
+        // if setter in position 1,2,3 (generate a number between 4,5,6 to decide which backcourt player digs)
+
+        // set team 2
+        // setter in team 2 attempts to set
+
+        // spiker
+        // if setter in position 1,2,3 (generate a number between 1,2,3 to decide which front court player spikes except setter) 
+        // if setter in position 4,5,6 (generate a number between 1,2,3 to decide which frontcourt player spikes)
     }
 
 }
