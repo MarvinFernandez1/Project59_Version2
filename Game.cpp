c8e6bbe9c3ca97eaeaefe7de645a57012cfe81f1
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-05-15 20:51:34 +0930

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -36,7 +36,45 @@ while(scoreTeam1 < winPoints && scoreTeam2 < winPoints) {
     bool action = true;
 
     while(action == true) {
-        Team* servingTeam = team1
+        Team* servingTeam = team1;
+    // intj;
+    // servingTeam team[j]=attemptServe()
+    //j++;
+    // while(ball = in){
+//         start simming
+    } 
+    else{
+        Team* servingTeam = team2;
+    }
     }
 
-}
+//     READ AND WRITE CODEBELOW
+    //takes input and creates a new file that can be opened
+// void .H::NewFile(string newFileName){
+//     cout<<"Enter desired text\n";
+//     cin>>input;
+//     ofstream outFile(newFileName);
+//     outFile << input; //input what you want to put into new file
+//     outFile.close();
+// }
+    
+    //function that creates file
+// void .H::ReadFile(string file){
+//     ifstream inFile(file);
+//     if(inFile.is_open()){
+//         while(getline (inFile,readFile)){
+//         cout<< readFile <<endl;
+//     }
+//     inFile.close();
+//     }
+//     else{
+//         cout << "unable to open file" << endl; 
+//     }
+// }
+    
+//        public:
+    //sets data members
+//     string fileName;
+//     string newFileName;
+//     string readFile;
+//     string input;
